57c266d948ee802335df6b05962b574202f2468c
せっきー <moto56@gmail.com>
2018-10-18 20:58:12 +0900

    cho-han game

---
+++ def_chohan.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# Your code here!
+# coding: utf-8
+# Your code here!
+import random
+def god_knows():
+    x = random.randint(1,10)
+   # print(x)
+    if x % 2 == 0:
+        print("偶数「丁」")
+    else:
+        print("奇数「半」")
+
+god_knows()
